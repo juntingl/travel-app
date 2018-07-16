ade8a54191d05612024406d734a72138a2603725
Junting <342766475@qq.com>
2018-07-16 14:35:07 +0800

    index recommend - 首页热销推荐组件

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of recommendList"
+        :key="item.id"
+      >
+          <img class="item-img" :src="item.imgUrl" alt="">
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1412/9f/fbff2a5302c0ce906ef9b64159730f89.water.jpg_200x200_87806940.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫得海洋主题乐园'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1412/9f/fbff2a5302c0ce906ef9b64159730f89.water.jpg_200x200_87806940.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫得海洋主题乐园'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1412/9f/fbff2a5302c0ce906ef9b64159730f89.water.jpg_200x200_87806940.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫得海洋主题乐园'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1412/9f/fbff2a5302c0ce906ef9b64159730f89.water.jpg_200x200_87806940.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫得海洋主题乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+
+.title
+  margin-top: .2rem
+  line-height: .8rem
+  background: #eee
+  text-indent: .2rem
+.item
+  overflow: hidden
+  display: flex
+  height: 1.9rem
+  .item-img
+    width: 1.7rem
+    height: 1.7rem
+    padding: .1rem
+  .item-info
+    flex: 1
+    padding: .1rem
+    min-width: 0
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: 0.4rem
+      color: #ccc
+      ellipsis()
+    .item-button
+      margin-top: .16rem
+      line-height: .44rem
+      background: #ff9300
+      padding: 0 .1rem
+      border-radius: .06rem
+      color: #fff
+</style>
