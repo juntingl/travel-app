aef94584b1db310beda34fea91d99181202f131e
Junting <342766475@qq.com>
2018-07-16 15:07:30 +0800

    index - weekend 周末去哪儿组件‘

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of weekendList"
+        :key="item.id"
+      >
+          <div class="item-img-wrapper">
+            <img class="item-img" :src="item.imgUrl" alt="">
+          </div>
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+          </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
+          title: '上海必游TOP10',
+          desc: '中西合璧，现代和传统各有各的精彩'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1603/ad/69b0fc5f48ad5f.jpg_r_640x214_b0fd5c7b.jpg',
+          title: '亲子时光TOP10',
+          desc: '走进自然或玩转乐园，感知大千世界'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1511/1d/fe098e8cfec117.jpg_r_640x214_a9be292a.jpg',
+          title: '上海城市观光',
+          desc: '来这里寻找你所认识的上海'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7b/2a6b0ce087f0e5.jpg_r_640x214_5c76b115.jpg',
+          title: '情迷博物馆',
+          desc: '穿梭时光岁月寻找答案'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+
+.title
+  margin-top: .2rem
+  line-height: .8rem
+  background: #eee
+  text-indent: .2rem
+
+.item-img-wrapper
+  overflow: hidden
+  height: 0
+  padding-bottom: 33.9%
+  .item-img
+    width: 100%
+
+.item-info
+  padding: .1rem
+  min-width: 0
+  .item-title
+    line-height: .54rem
+    font-size: .32rem
+    ellipsis()
+  .item-desc
+    line-height: 0.4rem
+    color: #ccc
+    ellipsis()
+</style>
