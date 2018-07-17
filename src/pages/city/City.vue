6766cfd3230146e8106f48d00a82df764960127d
Junting <342766475@qq.com>
2018-07-17 11:31:06 +0800

    City - Search 组件布局

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="city">
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
-import CityHeader from './components/header'
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
