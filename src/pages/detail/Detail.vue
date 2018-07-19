9c396db7771a8fc215f4527fdcde966b3c8278c8
Junting <342766475@qq.com>
2018-07-19 17:29:18 +0800

    detail-header 详情页，头部布局和展示效果样式逻辑编写

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div style="height: 600px"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 
 export default {
   name: 'Detail',
   components: {
+    DetailHeader,
     DetailBanner
   }
 }
