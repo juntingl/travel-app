c7b045da7e6ac7699fb5ddeb0f1f4c53157002b3
Junting <342766475@qq.com>
2018-07-19 16:31:46 +0800

    detail-Banner-Gallary，组件布局和实现效果

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="(item, index) in imgs" :key="index">
+          <img class="gallary-img" :src="item" alt="">
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommoonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observer: true,
+        observeParents: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('handleGallaryClick')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  // 样式穿透
+  .container >>> .swiper-container
+    overflow: inherit
+
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+    background: #000
+    .wrapper
+      width: 100%
+      height: 0
+      padding-bottom: 100%
+      .gallary-img
+        width: 100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+</style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,21 +1,47 @@
 <template>
-  <div class="banner">
-    <img
-      class="banner-img"
-      src="http://img1.qunarzz.com/sight/p0/201309/27/661acb22614eae2bc8d65eac.jpg_600x330_2200e5bf.jpg" alt="">
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img
+        class="banner-img"
+        src="http://img1.qunarzz.com/sight/p0/201309/27/661acb22614eae2bc8d65eac.jpg_600x330_2200e5bf.jpg" alt="">
 
-    <div class="banner-info">
-      <div class="banner-title">大连圣亚海洋世界（AAA景点）</div>
-      <div class="banner-number">
-        <span class="iconfont banner-icon">&#xe678;</span> 39
+      <div class="banner-info">
+        <div class="banner-title">大连圣亚海洋世界（AAA景点）</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe678;</span> 39
+        </div>
       </div>
     </div>
+    <common-gallay
+      :imgs="imgs"
+      v-show="showGallary"
+      @handleGallaryClick="handleGallaryClose"
+    ></common-gallay>
   </div>
 </template>
 
 <script>
+import CommonGallay from 'common/gallary/Gallary'
+
 export default {
-  name: 'DetailBanner'
+  name: 'DetailBanner',
+  components: {
+    CommonGallay
+  },
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/201309/27/661acb22614eae2bc8d65eac.jpg_r_800x800_f2b5b8c3.jpg', 'http://img1.qunarzz.com/sight/p0/201309/27/b41a33ab5bf755bac8d65eac.jpg_r_800x800_f97a3cb5.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
 }
 </script>
 
