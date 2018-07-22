fa14891e885971a5afe448c2a0ef8846d0e660d5
Junting <342766475@qq.com>
2018-07-22 10:42:34 +0800

    detail

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,19 +2,38 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div style="height: 600px"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailHeader,
-    DetailBanner
+    DetailBanner,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票'
+        },
+        {
+          title: '学生票'
+        },
+        {
+          title: '儿童票'
+        }
+      ]
+    }
   }
 }
 </script>
