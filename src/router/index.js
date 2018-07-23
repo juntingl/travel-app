8ad6470faea11bd8c3f53c35f2d844ab596b1977
Junting <342766475@qq.com>
2018-07-23 15:44:02 +0800

    异步组件，按需加载对项目庞大的时候，提高性能优化而使用

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
+// import Home from '@/pages/home/Home'
+// import City from '@/pages/city/City'
+// import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -11,17 +11,18 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      // 异步组件，按需加载；app.js 文件小的时候不推荐使用异步组件
+      component: () => import('@/pages/home/Home')
     },
     {
       path: '/city',
       name: 'City',
-      component: City
+      component: () => import('@/pages/city/City')
     },
     {
       path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => import('@/pages/detail/Detail')
     }
   ],
   // 路由切换的时候回到最顶部
